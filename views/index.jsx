ca5d73bacf519550269ac3e0504413ce5be63fa6
unknown <nolan@thewellsfamily.us>
2023-03-12 12:23:54 -0700

    sending index and show data

---
+++ views/index.jsx
@@ -0,0 +1,23 @@
+const React = require('react')
+const bread = require('../models/bread')
+const Default = require('./layouts/default')
+
+function Index ({breads}) {
+    return (
+      <Default>
+        <h2>Index Page</h2>
+        {/* <p> I rock with {breads[0].name} bread! </p> */}
+        <ul>
+            {
+                breads.map((bread, index) =>{
+                    return(<li key={index}>
+                        <a href={`/breads/${index}`}>{bread.name}</a>
+                    </li>)
+                })
+            }
+        </ul>
+      </Default>
+    )
+}
+
+module.exports = Index
